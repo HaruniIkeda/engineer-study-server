1489c92cd54be96a7ef7c9a5e8fe9eefa33cec3c
HaruniIkeda <Ikeda@haru-ni.net>
2024-05-08 13:27:06 +0900

    update logic

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const express = require("express");
 const app = express();
 const Redis = require("ioredis");
 
-const redis = new Redis({
-  host: "matsushita-test.gpddhc.clustercfg.apse2.cache.amazonaws.com",
-  port: 6379,
-  password: null,
-  db: 0
-});
+let redis = null;
 
-// app.get("/", (req, res) =>{
-//   res.send("Hello World!");
-// });
+app.get("/redis/", async (req, res) => {
+  redis = redis ? redis : new Redis({
+    host: "matsushita-test.gpddhc.clustercfg.apse2.cache.amazonaws.com",
+    port: 6379,
+    password: null,
+    db: 0
+  });
 
-app.get("/", async (req, res) => {
   let count = await redis.get("count");
   if (count == null) {
     count = 1;
@@ -23,6 +21,10 @@ app.get("/", async (req, res) => {
   let result = "Access Count: " + count;
   console.log(result)
   res.send(result);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen("3000");
